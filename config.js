56fe9c19a5b10a96dfc0fcced7aaafc0946a8378
MagicalBoy <64646403+dung8466@users.noreply.github.com>
2023-08-05 23:39:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -26,14 +26,14 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '87501443f05fdb8dc966f9f71fbe7138', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '21.027763',
+	defaultLongitude: '105.834160',
 
 	// Autochange
 	autoChangeTheme: true,
@@ -83,9 +83,9 @@ const CONFIG = {
 		},
 		{
 			id: '6',
-			name: 'Odysee',
-			icon: 'youtube',
-			link: 'https://odysee.com/',
+			name: '4chan',
+			icon: 'clover',
+			link: 'https://www.4chan.org/index.php',
 		},
 	],
 
